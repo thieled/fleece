aa84f5bb4e257d7dc5adebbb89a46393f84aff4d
thieled <daniel.thiele@fu-berlin.de>
2024-02-08 19:51:20 +0100

    changed rbind to tibblify

--- R/rectangularize.R
+++ R/rectangularize.R
@@ -1,7 +1,7 @@
 #' Bind JSON data parsed as an R list to a data frame and unnest nested columns
 #'
-#' This function takes JSON data parsed as an R list, binds it to a data frame, and
-#' then recursively applies the "unnest_recursively" function to unnest any nested
+#' This function takes JSON data parsed as an R list, binds it to a tibble usign tibblify::tibblify,
+#'  and then recursively applies the "unnest_recursively" function to unnest any nested
 #' list columns in the data frame. The input data can contain either a single observation
 #' (a list representing a row) or multiple observations (a list of lists representing
 #' multiple rows).
@@ -22,18 +22,46 @@ rectangularize <- function(content){
     stop("Error: Please provide content in list format.")
   }
 
-    # Check if content contains 1 observation (=> simple rbind; 1 row df)
-    if(all(sapply(content, is.list))){
+  # Check if content contains 1 observation (=> simple rbind; 1 row df)
+  if(all(sapply(content, is.list))){
 
-      con <- do.call(rbind, content) |> tibble::as_tibble()
+    # con <- do.call(rbind, content) |> tibble::as_tibble()
+    con <- tibblify::tibblify(content)
+    con <- unnest_recursively(con)
+    return(con)
+
+  }else{ # ...or more (=> do.call(rbind, l); multiple rows df)
+
+    con <- rbind(content) |> tibble::as_tibble()
+    con <- unnest_recursively(con)
+
+    if(any(sapply(con, is.list))){ # Re-run
       con <- unnest_recursively(con)
-      return(con)
-
-    }else{ # ...or more (=> do.call(rbind, l); multiple rows df)
-
-      con <- rbind(content) |> tibble::as_tibble()
-      con <- unnest_recursively(con)
-      return(con)
     }
 
+    return(con)
+  }
+
 }
+
+# rectangularize <- function(content){
+#
+#   # Stop if content is not a list
+#   if (!is.list(content)){
+#     stop("Error: Please provide content in list format.")
+#   }
+#
+#   # Check if content contains 1 observation (=> simple rbind; 1 row df)
+#   if(all(sapply(content, is.list))){
+#
+#     con <- do.call(rbind, content) |> tibble::as_tibble()
+#     con <- unnest_recursively(con)
+#     return(con)
+#
+#   }else{ # ...or more (=> do.call(rbind, l); multiple rows df)
+#
+#     con <- rbind(content) |> tibble::as_tibble()
+#     con <- unnest_recursively(con)
+#     return(con)
+#   }
+#
